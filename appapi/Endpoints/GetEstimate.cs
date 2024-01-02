1b6f6a423009c528dc53d4c3de056568036717d8
aj <anthonyfields1@gmail.com>
2024-01-02 07:07:43 -0500

    Estimate updates

--- appapi/Endpoints/GetEstimate.cs
+++ appapi/Endpoints/GetEstimate.cs
@@ -9,7 +9,6 @@ static class GetEstimate
         // validate inputs
         try
         {
-<<<<<<< HEAD:appapi/Endpoints/HousingEstimate.cs
             var query = db.RequestEvents
                 .Where(r => r.RequestTimeUTC >= DateTime.UtcNow.AddDays(-91))
                 .Include(
@@ -28,18 +27,6 @@ static class GetEstimate
             var count = zestimates.Count();
             var estimate = count != 0 ? zestimates.Average() : 0;
             return TypedResults.Ok(new { estimate, count, message = "Success" });
-=======
-            var averageHomeValue = await db.HousingDetails
-                .Where(h => h.Bedrooms == numBeds
-                && h.ZipCode == zip.ToString()
-                && h.Bathrooms == numBaths
-                && h.LotAreaUnit == "acres"
-                && h.LotAreaValue > lotSize
-                && h.Zestimate != null)
-                .Select(h => h.Zestimate)
-                .AverageAsync();
-            return TypedResults.Ok(averageHomeValue);
->>>>>>> 4918bbefa18aee130e99b946457141893f7dcf4a:appapi/Endpoints/GetEstimate.cs
         }
         catch (Exception ex)
         {
